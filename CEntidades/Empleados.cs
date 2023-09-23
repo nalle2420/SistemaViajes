1ef4694f56e3f264ef0dbe749977b0403bf91d9f
Nallely Reyes <na_reyesd@unicah.edu>
2023-09-23 00:40:00 -0600

    Entidad Empleado, Usuario, Puesto y Permiso

---
+++ CEntidades/Empleados.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CEntidades
+{
+    public class Empleados
+    {
+        public int Emp_ID { get; set; }
+
+        public string Emp_identidad { get; set; }
+
+        public string Emp_Nombre { get; set; }
+
+        public string Emp_Apellido { get; set; }
+
+        public string Emp_Direccion { get; set; }
+
+        public string Emp_Telefono { get; set; }
+
+        public Puesto oPuesto { get; set; }
+
+        public bool Emp_transporte { get; set; }
+    }
+}
